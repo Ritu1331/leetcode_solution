2f210085d6c6039b26246eeb4fe4e8b5187f0512
Ritu1331 <pritu1070@gmail.com>
2026-07-01 18:01:54 +0530

    Time: 281 ms (10.61%), Space: 29.4 MB (24.9%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,34 +1,50 @@
 class Solution(object):
+
+    # Direction arrays (Up, Right, Down, Left)
+    x = [-1, 0, 1, 0]
+    y = [0, 1, 0, -1]
+
+    def valid(self, i, j, n, m):
+        if i < 0 or i >= n or j < 0 or j >= m:
+            return False
+        return True
+
+    def dfs(self, grid, n, m, i, j, vis):
+
+        vis[i][j] = True
+
+        for k in range(4):
+
+            row = i + self.x[k]
+            col = j + self.y[k]
+
+            if (self.valid(row, col, n, m) and
+                grid[row][col] == "1" and
+                not vis[row][col]):
+
+                self.dfs(grid, n, m, row, col, vis)
+
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
+
         if not grid:
             return 0
-        
-        rows, cols = len(grid), len(grid[0])
+
+        n = len(grid)
+        m = len(grid[0])
+
+        vis = [[False] * m for _ in range(n)]
+
         count = 0
-        
-        def dfs(r, c):
-            # boundary + water check
-            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
-                return
-            
-            # mark visited
-            grid[r][c] = "0"
-            
-            # explore 4 directions
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c+1)
-            dfs(r, c-1)
-        
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1":
+
+        for i in range(n):
+            for j in range(m):
+
+                if grid[i][j] == "1" and not vis[i][j]:
                     count += 1
-                    dfs(r, c)
-        
+                    self.dfs(grid, n, m, i, j, vis)
+
         return count
-        
