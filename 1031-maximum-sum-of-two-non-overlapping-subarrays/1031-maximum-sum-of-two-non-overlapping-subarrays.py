6d375b0d8d0fd7c054a9eab84c72e9fd902c978d
Ritu1331 <pritu1070@gmail.com>
2026-07-14 19:41:38 +0530

    Time: 3 ms (69.23%), Space: 12.6 MB (7.69%) - LeetHub

--- 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
@@ -5,7 +5,7 @@ class Solution(object):
         n = len(nums)
 
         # Prefix sum
-        prefix = [0] * (n + 1)
+        prefix = [0] * (n + 1 )
 
         for i in range(n):
             prefix[i + 1] = prefix[i] + nums[i]
