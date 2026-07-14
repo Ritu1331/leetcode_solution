56c0416419fbe9b9526e09856b4e263de48c2589
Ritu1331 <pritu1070@gmail.com>
2026-07-14 19:23:01 +0530

    Time: 0 ms (100%), Space: 12.4 MB (88.46%) - LeetHub

---
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.py
@@ -0,0 +1,37 @@
+class Solution(object):
+
+    def helper(self, nums, L, M):
+
+        n = len(nums)
+
+        # Prefix sum
+        prefix = [0] * (n + 1)
+
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + nums[i]
+
+        # Sum of first L elements
+        maxL = prefix[L] - prefix[0]
+
+        ans = 0
+
+        for i in range(L + M, n + 1):
+
+            # Best L-length subarray before current M
+            left_sum = prefix[i - M] - prefix[i - M - L]
+
+            maxL = max(maxL, left_sum)
+
+            # Current M-length subarray
+            currM = prefix[i] - prefix[i - M]
+
+            ans = max(ans, maxL + currM)
+
+        return ans
+
+    def maxSumTwoNoOverlap(self, nums, firstLen, secondLen):
+
+        return max(
+            self.helper(nums, firstLen, secondLen),
+            self.helper(nums, secondLen, firstLen)
+        )
