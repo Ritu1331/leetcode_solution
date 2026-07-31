b831ca70f36c1d15d79a593d24064dd3687a47a7
Ritu1331 <pritu1070@gmail.com>
2026-07-31 12:13:10 +0530

    Time: 263 ms (77.78%), Space: 13 MB (27.78%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def minimumPushes(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        freq = {}
+
+        # Count frequency
+        for ch in word:
+            freq[ch] = freq.get(ch, 0) + 1
+
+        # Sort frequencies
+        frequencies = sorted(freq.values(), reverse=True)
+
+        ans = 0
+
+        for i in range(len(frequencies)):
+            pushes = (i // 8) + 1
+            ans += frequencies[i] * pushes
+
+        return ans
+        
