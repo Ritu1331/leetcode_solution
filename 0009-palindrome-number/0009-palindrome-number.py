67f01bf2786cc7d2d10dcb43243e1f1c62bcd361
Ritu1331 <pritu1070@gmail.com>
2026-08-04 04:29:40 +0530

    Time: 11 ms (55.46%), Space: 12.4 MB (17.99%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        s = str(x)
+        '''s = str(x)
 
         i = 0
-        j = len(s) - 1
+        j = len(s) - 1    ### tc and sc = o(n)
 
         while i < j:
             if s[i] != s[j]:
@@ -16,7 +16,22 @@ class Solution(object):
             i += 1
             j -= 1
 
-        return True
+        return True'''
+
+        if x < 0 or( x % 10 == 0 and x != 0):
+            return False
+        
+        reverse = 0
+
+        while x > reverse:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x = x // 10
+
+        if x == reverse or x == reverse // 10:
+            return True 
+        
+        return False 
 
 
 
