9cb9597d34f4409bd1e6378e22a4cf31ed894d6a
Ritu1331 <pritu1070@gmail.com>
2026-08-03 08:44:29 +0530

    Time: 48 ms (59.16%), Space: 12.4 MB (61.41%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        left = 0
+        right = int(c ** 0.5)
+
+        while left <= right :
+            total = left ** 2 + right ** 2
+
+            if total == c:
+                return True
+            
+            if total < c:
+                left += 1
+            else:
+                right -= 1
+        
+        return False
+
+        
