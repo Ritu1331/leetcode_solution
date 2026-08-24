8bfdd950fa8738f45949e49aca3c50d1e1f27913
Ritu1331 <pritu1070@gmail.com>
2026-08-24 17:33:07 +0530

    Time: 7 ms (36.55%), Space: 14 MB (29.13%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 0
+        for i in range(len(nums)):
+            ans = ans ^ nums[i]
+        
+        return ans 
+
+        
