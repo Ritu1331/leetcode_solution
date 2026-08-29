a31c8d7832a9da121d87de9868fa4ff4e6198126
Ritu1331 <pritu1070@gmail.com>
2026-08-29 14:20:15 +0530

    Time: 0 ms (100%), Space: 12.4 MB (54.35%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: int
         """
     
-        while len(stones) > 1:
+        """while len(stones) > 1:
             stones.sort()
 
             y = stones.pop()   # heaviest
@@ -16,5 +16,35 @@ class Solution(object):
 
         if stones:
             return stones[0]
+        return 0"""
+
+
+        import heapq
+        stones = [-stone for stone in stones]
+
+        # Convert list into heap
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+
+            # Get heaviest stone
+            stone1 = -heapq.heappop(stones)
+
+            # Get second heaviest stone
+            stone2 = -heapq.heappop(stones)
+
+            # If weights are different
+            if stone1 != stone2:
+
+                # Difference is the remaining stone
+                new_stone = stone1 - stone2
+
+                # Add it back to heap
+                heapq.heappush(stones, -new_stone)
+
+        # If one stone remains, return it
+        if stones:
+            return -stones[0]
+
         return 0
         
