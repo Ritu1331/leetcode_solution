936054c0f5213814d9212efae213ff5bd847302f
Ritu1331 <pritu1070@gmail.com>
2026-08-29 14:26:14 +0530

    Time: 261 ms (13.48%), Space: 13.7 MB (24.98%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,51 @@
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+        import heapq
+
+        # Count frequency
+        freq = {}
+
+        for task in tasks:
+            freq[task] = freq.get(task, 0) + 1
+
+        # Max heap
+        heap = []
+
+        for count in freq.values():
+            heapq.heappush(heap, -count)
+
+        time = 0
+
+        while heap:
+
+            temp = []
+
+            # One cycle = n + 1
+            for i in range(n + 1):
+
+                if heap:
+                    count = -heapq.heappop(heap)
+                    count -= 1
+
+                    if count > 0:
+                        temp.append(count)
+
+                    time += 1
+
+                elif temp:
+                    time += 1
+
+                else:
+                    break
+
+            # Put remaining tasks back
+            for count in temp:
+                heapq.heappush(heap, -count)
+
+        return time
+        
