35f1c22ccec6019d1ad268ed0450dc9db85f60f8
Ritu1331 <pritu1070@gmail.com>
2026-08-30 18:05:42 +0530

    Time: 197 ms (69.65%), Space: 16.2 MB (48.28%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -0,0 +1,20 @@
+import heapq
+
+class Solution(object):
+    def scheduleCourse(self, courses):
+
+        courses.sort(key=lambda x: x[1])
+
+        max_heap = []
+        time = 0
+
+        for duration, lastDay in courses:
+
+            time += duration
+            heapq.heappush(max_heap, -duration)
+
+            if time > lastDay:
+                longest = -heapq.heappop(max_heap)
+                time -= longest
+
+        return len(max_heap)
