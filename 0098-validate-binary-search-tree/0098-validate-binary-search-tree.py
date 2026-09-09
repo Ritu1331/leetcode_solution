25f4d409498c3c98be2b75f085ab0651d3d10d61
Ritu1331 <pritu1070@gmail.com>
2026-09-09 10:18:00 +0530

    Time: 7 ms (8.9%), Space: 17.5 MB (8.41%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -31,8 +31,26 @@ class Solution(object):
                 self.prev = root
 
             # Step 3 : Visit right subtree
-            inorder(root.right)
+            inorder(root.right)   # tc = n , sc = h
 
         inorder(root)
 
         return self.ans
+
+        '''arr = []
+
+def inorder(root):
+    if root is None:
+        return
+
+    inorder(root.left)
+    arr.append(root.val)
+    inorder(root.right)
+
+inorder(root)
+
+for i in range(1, len(arr)):    # tc = n , sc -- n
+    if arr[i] <= arr[i - 1]:
+        return False
+
+return True'''
